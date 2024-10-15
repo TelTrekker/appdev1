82ee0ad94c1602de56472627d6bb470a62946c22
Krystel <krystelmagpayo@student.laverdad.edu.ph>
2024-10-15 21:02:00 +0800

    I'm done completing the section on rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,22 +1,11 @@
 import './App.css'
-import AdminPanel from './AdminPanel';
-import LoginForm from './LoginForm';
+import ShoppingList from './ShoppingList'
 
 function App() {
-  let content;
-  let isLoggedIn = false;
-
-  if (isLoggedIn){
-    content = <AdminPanel />
-  } else {
-    content = <LoginForm />
-  }
-  
-
+ 
   return (
     <>
-      <h1>Welcome to my app</h1>
-      {content}
+    <ShoppingList />
     </>
   )
 }
